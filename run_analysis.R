0194aef1590b703457de2d6b992aae05954286a9
Yash Gupta <y.gupta32@gmail.com>
2018-08-05 23:01:35 +0530

    Added course project script

---
+++ run_analysis.R
@@ -0,0 +1,81 @@
+#load libraries
+library(dplyr)
+
+########################
+## Downoad and check the files
+dataurl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zipName <- "RawDataset"
+
+if(!file.exists(zipName)){
+        download.file(dataurl,zipName)
+}
+
+if(!file.exists("UCI HAR Dataset")) {
+        unzip(zipName)
+}
+######################
+
+
+#################################
+#load activities and datasets
+setwd("UCI HAR Dataset")
+activityLabels <- read.table("activity_labels.txt")
+features <- read.table("features.txt")
+
+# Convert factor type activity and features in character form
+activityLabels[,2]=as.character(activityLabels[,2])
+features[,2] <- as.character(features[,2])
+####################################
+
+
+###################################
+#Select the desired features
+RequiredFeatures <- grep("mean|std", features[,2])
+RequiredFeaturesNames <- features[RequiredFeatures,2] %>%
+        gsub(pattern = "-mean",replacement = "Mean") %>%
+        gsub(pattern = "-std",replacement = "Std") %>%
+        gsub(pattern = "[-()]",replacement = "")
+View(RequiredFeaturesNames)
+##################################
+
+##################################
+# Merge the datasets
+train <- read.table("train/X_train.txt")[RequiredFeatures]
+trainActivities <- read.table("train/Y_train.txt")
+trainSubjects <- read.table("train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+test <- read.table("test/X_test.txt")[RequiredFeatures]
+testActivities <- read.table("test/Y_test.txt")
+testSubjects <- read.table("test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
+###################################
+
+###################################
+# merge datasets and add labels
+MergedData <- rbind(train, test)
+colnames(MergedData) <- c("subject", "activity", RequiredFeaturesNames)
+View(MergedData)
+###################################
+
+###################################
+#cleanup Memory
+rm(test,train)
+rm(testActivities,trainActivities,testSubjects,trainSubjects)
+
+#make the activity and subject as factors
+MergedData$activity <- factor(MergedData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+MergedData$subject <- as.factor(MergedData$subject)
+###################################
+
+###################################
+# group by subject and activity and summarise using mean
+humanActivityMeans <- MergedData %>% 
+        group_by(subject, activity) %>%
+        summarise_all(.funs=mean)
+View(humanActivityMeans)
+
+#write back to data to table
+write.table(humanActivityMeans,file="tidydata.txt",
+            row.names = FALSE, quote=FALSE)
+###################################
